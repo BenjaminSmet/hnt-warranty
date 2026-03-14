a9fab2787f1dd96c213bd42438b948a37e3bd688
BenjaminSmet <benjamin.smet29@gmail.com>
2026-03-14 20:37:37 +0100

    Initial setup - React + Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBKiyC-f0Uvazwjy-lbsHGlpgp88w75p3s",
-  authDomain: "hnt-warranty.firebaseapp.com",
-  projectId: "hnt-warranty",
-  storageBucket: "hnt-warranty.firebasestorage.app",
-  messagingSenderId: "674544810305",
-  appId: "1:674544810305:web:8c8578c8eddcad04b309c0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
