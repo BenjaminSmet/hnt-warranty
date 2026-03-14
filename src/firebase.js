9cf5b8294cf09776a09b4797f396f64ed6b63adb
BenjaminSmet <benjamin.smet29@gmail.com>
2026-03-14 20:35:03 +0100

    Initial setup - React + Firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBKiyC-f0Uvazwjy-lbsHGlpgp88w75p3s",
+  authDomain: "hnt-warranty.firebaseapp.com",
+  projectId: "hnt-warranty",
+  storageBucket: "hnt-warranty.firebasestorage.app",
+  messagingSenderId: "674544810305",
+  appId: "1:674544810305:web:8c8578c8eddcad04b309c0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
